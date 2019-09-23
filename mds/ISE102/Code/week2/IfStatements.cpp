f9b8de2473441d8412686f6bbff8b2cc3012fc94
DESKTOP-HOME\Danny <danny.mcgillick@gmail.com>
2019-09-23 18:47:13 +1000

    Last second class demo

---
+++ mds/ISE102/Code/week2/IfStatements.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool isRaining = true;
+
+    if (isRaining)
+    {
+        // nothing
+        // more
+        cout << "It's raining";
+    }  
+    else 
+    {
+        cout << "sunbathe";
+    }  
+
+    while(isRaining)
+    {
+        // do this over and over
+    }
+
+    return(0);
+}
